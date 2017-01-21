ec001590ab2a41562ce91cbc06e6fecbcdfa1b22
takuya310 <takuya310@gmail.com>
2017-01-21 20:59:56 +0900

    focus on Ikoma city hall

--- Ikoma_cesium-starter-app-master/Source/App.js
+++ Ikoma_cesium-starter-app-master/Source/App.js
@@ -1,2 +1,6 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
+
 viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Ikoma_cesium-starter-app-master/Source/civictechparty.kml'));
+
+viewer.camera.flyTo({
+     destination : Cesium.Cartesian3.fromDegrees(135.700553,34.691979,8000.0)});
