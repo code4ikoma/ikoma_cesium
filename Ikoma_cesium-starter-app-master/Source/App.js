6f790c8a8c6c9f06edaa8fdd5afd9788fcd943c1
takuya310 <takuya310@gmail.com>
2017-01-21 20:56:02 +0900

    change kml path

--- Ikoma_cesium-starter-app-master/Source/App.js
+++ Ikoma_cesium-starter-app-master/Source/App.js
@@ -1,2 +1,2 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
-viewer.dataSources.add(Cesium.KmlDataSource.load('civictechparty.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Ikoma_cesium-starter-app-master/Source/civictechparty.kml'));
