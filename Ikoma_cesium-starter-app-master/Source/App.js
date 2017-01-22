049981d0d92d1d98d724b2b115a12415bc0377e3
takuya310 <takuya310@gmail.com>
2017-01-22 22:04:02 +0900

    modify kml values and icons, height of first view

--- Ikoma_cesium-starter-app-master/Source/App.js
+++ Ikoma_cesium-starter-app-master/Source/App.js
@@ -3,4 +3,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Ikoma_cesium-starter-app-master/Source/civictechparty.kml'));
 
 viewer.camera.flyTo({
-     destination : Cesium.Cartesian3.fromDegrees(135.700553,34.691979,8000.0)});
+     destination : Cesium.Cartesian3.fromDegrees(135.700553,34.691979,6000.0)});
