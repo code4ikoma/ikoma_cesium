073f384061ab5d5df20ca0f177132ee4398c9ef2
takuya310 <takuya310@gmail.com>
2017-01-22 23:50:14 +0900

    retry kml path

--- Source/App.js
+++ Source/App.js
@@ -1,6 +1,7 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Source/civictechparty.kml'));
+///viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Source/civictechparty.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://cesium.code4ikoma.org/Source/civictechparty.kml'));
 
 viewer.camera.flyTo({
      destination : Cesium.Cartesian3.fromDegrees(135.697856,34.68636,4000.0)});
