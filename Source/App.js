4ac466716d6e494e8d4eadcf4a4bd4c07b5b1c1c
takuya310 <takuya310@gmail.com>
2017-01-22 23:36:16 +0900

    change kml path

--- Source/App.js
+++ Source/App.js
@@ -1,6 +1,6 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('civictechparty.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Source/civictechparty.kml'));
 
 viewer.camera.flyTo({
      destination : Cesium.Cartesian3.fromDegrees(135.700553,34.691979,4000.0)});
