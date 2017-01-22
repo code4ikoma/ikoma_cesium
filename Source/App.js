46505fdc3e79c725ede598885b61e682701e8ec6
takuya310 <takuya310@gmail.com>
2017-01-22 23:44:29 +0900

    retry cname

--- Source/App.js
+++ Source/App.js
@@ -3,4 +3,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load('https://code4ikoma.github.io/ikoma_cesium/Source/civictechparty.kml'));
 
 viewer.camera.flyTo({
-     destination : Cesium.Cartesian3.fromDegrees(135.700553,34.691979,4000.0)});
+     destination : Cesium.Cartesian3.fromDegrees(135.697856,34.68636,4000.0)});
